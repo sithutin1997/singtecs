50068dfa5daa94ca439513b3fe0dd9abf1f48019
sithutin1997 <sithu5897@gmail.com>
2023-06-16 11:38:50 +0630

    feat: service hover dropdown menu

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -12,66 +12,62 @@ const DropDownMenu = ({show}) => {
   }
   const [visible, setVisible] = useState(false)
 
+  useEffect(() => {
+    handleMouseEnter()
+  }, [show])
+
+
   const handleMouseEnter = (value) => {
-    if(show == false && value == false) {
+   if (show == false && value == false) {
       setVisible(false)
     } else {
       setVisible(true)
     }
   }
 
-  // useEffect(() => {
-  //   if(show == false) {
-  //     setTimeout(() => {
-  //       setVisible(false)
-  //     }, 300);
-  //   } else {
-  //     setVisible(true)
-  //   }
-  //   console.log(show)
-  // }, [show])
   
   return (
-    <motion.div 
-      initial={false}
-      animate={show ? 'open' : 'closed'}
-      onMouseEnter={handleMouseEnter(true)} onMouseLeave={handleMouseEnter(false)} 
-      variants={{
-        open: {
-          opacity: 1,
-          x: 0 ,
-          clipPath: "inset(0% 0% 0% 0% round 10px)",
-          transition: {
-            type: "spring",
-            bounce: 0,
-            duration: 0.7,
-            delayChildren: 0.3,
-            staggerChildren: 0.05
+      <motion.div 
+        onMouseEnter={()=> handleMouseEnter(true)} onMouseLeave={()=> handleMouseEnter(false)}
+        animate= {visible ? 'open' : 'closed'}
+        initial={false}
+        variants={{
+          open: {
+            opacity: 1,
+            x: 0 ,
+            clipPath: "inset(0% 0% 0% 0% round 10px)",
+            transition: {
+              type: "spring",
+              bounce: 0,
+              duration: 0.7,
+              delayChildren: 0.3,
+              staggerChildren: 0.05
+            }
+          },
+          closed: {
+            clipPath: "inset(10% 50% 90% 50% round 10px)",
+            transition: {
+              type: "spring",
+              bounce: 0,
+              duration: 0.3
+            }
           }
-        },
-        closed: {
-          clipPath: "inset(10% 50% 90% 50% round 10px)",
-          transition: {
-            type: "spring",
-            bounce: 0,
-            duration: 0.3
-          }
-        }
-      }}
-      className={visible ? `absolute top-12 w-full bg-black right-0 z-10 mt-12 h-2/5 flex flex-row ps-32  py-8 space-x-12` : `hidden`}>
-      <motion.div className='flex flex-col text-left mt-8 me-12 space-y-4'>
-        <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>Custom Web & Mobile Development</motion.span>
-        <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>Digital Transformation</motion.span>
-        <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>CX/UX Engineering</motion.span>
-        <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>IoT and Cloud Computing</motion.span>
+        }}
+        className={visible ? `absolute top-12 w-full bg-black right-0 z-10 mt-6 h-2/5 flex flex-row ps-32  py-8 space-x-12` : `hidden`}>
+        <motion.div className='flex flex-col text-left mt-8 me-12 space-y-4'>
+          <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>Custom Web & Mobile Development</motion.span>
+          <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>Digital Transformation</motion.span>
+          <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>CX/UX Engineering</motion.span>
+          <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>IoT and Cloud Computing</motion.span>
+        </motion.div>
+        <motion.div className='flex flex-col text-left mt-8 space-y-4'>
+          <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>Cyber Security</motion.span>
+          <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>IT Consultancy</motion.span>
+          <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>Enterprise Service</motion.span>
+          <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>Online Diet Consultant</motion.span>
+        </motion.div>
       </motion.div>
-      <motion.div className='flex flex-col text-left mt-8 space-y-4'>
-        <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>Cyber Security</motion.span>
-        <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>IT Consultancy</motion.span>
-        <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>Enterprise Service</motion.span>
-        <motion.span variants={itemVariants} className='text-xl text-white font-gotham-light'>Online Diet Consultant</motion.span>
-      </motion.div>
-    </motion.div>
+    
   )
 }
 
--- src/pages/layout/Header.js
+++ src/pages/layout/Header.js
@@ -11,8 +11,6 @@ import { useLocation,Link } from 'react-router-dom'
 import white from '../../assets/white-logo.svg'
 
 const Header = ({setShow}) => {
-  const location = useLocation()
-  const [visible,setVisible] = useState(false)
   
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
@@ -20,17 +18,6 @@ const Header = ({setShow}) => {
     setIsDropdownVisible(value)
   }
 
-  // const handleMouseEnter = () => {
-  //   setIsDropdownVisible(true);
-  // };
-
-  // const handleMouseLeave = () => {
-  //   setIsDropdownVisible(false);
-  // };
-
-  // const toggleSubmenu = () => {
-  //   setIsSubmenuOpen(!isSubmenuOpen);
-  // };
 
   return (
       <div className='flex flex-row items-center border-solid border-b border-b-white py-5'>
@@ -41,23 +28,17 @@ const Header = ({setShow}) => {
         </div>
         <div className='w-1/2'>
           <div className="flex flex-row justify-center gap-x-10 ">
-            <Link to={"/about"}className='text-white font-gotham-book cursor-pointer'>About Us</Link>
+            <Link to={"/about"} className='text-white font-gotham-book cursor-pointer'>About Us</Link>
 
-            <Link to={"/ourservices"}className='relative text-white font-gotham-book cursor-pointer' onMouseEnter={handleMouseEnter(true)} onMouseLeave={handleMouseEnter(false)}>Our Services</Link>
+            <Link to={"/ourservices"} className='relative text-white font-gotham-book cursor-pointer' onMouseEnter={()=>handleMouseEnter(true)} onMouseLeave={()=>handleMouseEnter(false)}>Our Services</Link>
             <DropDownMenu show={isDropdownVisible}/>
 
-            {/* <span className='text-white font-gotham-book cursor-pointer' onClick={()=> {setVisible(!visible); setShow(!visible)}}>Our Services</span> */}
-
-            <Link to={"/getintouch"}className='text-white font-gotham-book cursor-pointer'>Get a free quote</Link>
+            <Link to={"/getintouch"} className='text-white font-gotham-book cursor-pointer'>Get a free quote</Link>
             <img src={facebook} alt="" className='object-scale-down'/>
             <img src={whatsapp} alt="" className='object-scale-down'/>
             <img src={linkedin} alt="" className='object-scale-down'/>
           </div>
         </div>
-
-        {/* <DropDownMenu show={visible}/> */}
-        {/* <HoverTest show={visible}/> */}
-        
       </div>
   )
 }
